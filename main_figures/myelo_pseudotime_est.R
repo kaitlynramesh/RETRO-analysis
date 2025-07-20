9da78eb8329bf131b588aad43f47cf4b97ce3b77
kaitlynramesh <kaitlyn.ramesh@gmail.com>
2025-07-20 06:31:36 -0400

    file re-organization

---
+++ main_figures/myelo_pseudotime_est.R
@@ -0,0 +1,46 @@
+# Inferring pseudotime from ensemble MST scores from myocardial infarction dataset
+# FIGURES (??)
+
+library(RETRO)
+library(ggplot2)
+
+# load datasets
+myo_url = "https://raw.githubusercontent.com/kaitlynramesh/RETRO-analysis/main/real/scd_myo.rds"
+scd_myo = readRDS(gzcon(url(myo_url)))
+
+# load scoring information
+myo_scores = "https://raw.githubusercontent.com/kaitlynramesh/RETRO-analysis/main/benchmark/RETRO_scores_myo.rda"
+load(gzcon(url(myo_scores)))
+
+boxplot_scoring(retro_obj)
+
+retro_obj <- get_num_lineages(retro_obj, percent=0.1, cutoff=0.8) # top MST 
+retro_obj <- get_bezier_curve(retro_obj, extension=1) # spline fitting
+
+retro_pt_obj <- get_mapped_cells(retro_obj) # update lineage information
+retro_pt_obj <- pseudotime_fit(retro_pt_obj) # pseudotime estimation
+
+bcurves <- sapply(retro_pt_obj@RETRO_Curve, "[", 1)
+umap = retro_obj@coordinates
+time = retro_obj@time
+
+ggplot(as.data.frame(umap), aes(x=UMAP2L_1, y=UMAP2L_2, colour=as.factor(time))) + 
+  geom_point() + 
+  guides(colour=guide_legend(ncol=1)) + 
+  theme_bw() + labs(colour="time")
+
+pseudotime = retro_pt_obj@pseudotime
+
+ggplot(as.data.frame(umap), aes(x=UMAP2L_1, y=UMAP2L_2, colour=pseudotime)) + 
+  geom_point() + 
+  scale_colour_continuous(low="blue", high="orange") + 
+  geom_point(data = as.data.frame(bcurves[[1]][,1:2]), aes(x = V1, y = V2), size=0.5, colour='black') +
+  geom_point(data = as.data.frame(bcurves[[2]][,1:2]), aes(x = V1, y = V2), size=0.5, colour='black') +
+  geom_point(data = as.data.frame(bcurves[[3]][,1:2]), aes(x = V1, y = V2), size=0.5, colour='black') +
+  geom_point(data = as.data.frame(bcurves[[4]][,1:2]), aes(x = V1, y = V2), size=0.5, colour='black') +
+  theme_bw()
+
+dir = "~/KaitlynRRStudio/RETRO-analysis/benchmark/"
+saveRDS(retro_pt_obj, file=paste0(dir, "RETRO_PT_myo.rds"))
+
+
